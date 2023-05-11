acf72f1e5e3404cd7240fd76678bdef8b7a5fcfd
diniyuha <diniyuha@gmail.com>
2023-05-11 22:30:20 +0600

    добавлены сущности MovieSeasonEntity.cs и MovieEpisodeEntity.cs

--- OnlineCinema.Api/OnlineCinema.Data/Entities/UserEntity.cs
+++ OnlineCinema.Api/OnlineCinema.Data/Entities/UserEntity.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using OnlineCinema.Data.Enums;
+﻿using OnlineCinema.Data.Enums;
 
 namespace OnlineCinema.Data.Entities
 {
-    public class UserEntity
+    public class UserEntity : BaseEntity
     {
-        public Guid Id { get; set; }
-        
         public string Email { get; set; }
-        
+
         public string Password { get; set; }
-        
+
         public string? Name { get; set; }
-        
+
         public string? Surname { get; set; }
-        
+
         public string? Icon { get; set; }
-        
+
         public Role Role { get; set; }
     }
 }
