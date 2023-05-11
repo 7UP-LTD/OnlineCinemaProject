0683158a8ffa89eeb0798fedb2648a6ea86ce031
diniyuha <diniyuha@gmail.com>
2023-05-11 18:34:51 +0600

    добавлены сущности User и Movies

---
+++ OnlineCinema.Api/OnlineCinema.Data/Entities/MovieEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OnlineCinema.Data.Entities
+{
+    public class MovieEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// дата добавления на сайт
+        /// </summary>
+        public DateTime DateAdded { get; set; }
+
+        /// <summary>
+        /// год выпуска фильма
+        /// </summary>
+        public DateTime ReleaseYear { get; set; }
+
+        /// <summary>
+        /// ссылка на картинку-заставку
+        /// </summary>
+        public string MoviePoster { get; set; }
+
+        /// <summary>
+        /// сериал или нет
+        /// </summary>
+        public bool IsSeries { get; set; }
+    }
+}
