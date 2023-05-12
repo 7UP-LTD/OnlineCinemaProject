7e7cf2af9baaa649ae55bfd516efb95d77e0703e
senia1 <oks314@gmail.com>
2023-05-12 08:28:54 +0300

    added in Model class User.cs

---
+++ OnlineCinema.Api/OnlineCinema.Logic/Models/User.cs
@@ -0,0 +1,18 @@
+﻿
+namespace OnlineCinema.Logic.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime DateRegistered { get; set; }
+
+        public ICollection<MovieComments> MovieComment { get; set; }
+    }
+}
