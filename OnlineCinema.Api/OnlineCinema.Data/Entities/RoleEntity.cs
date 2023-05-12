627f249e52d559b48fb496f1bfb732e4748691f0
IliaKim91 <paterfamelias@gmail.com>
2023-05-13 02:47:27 +0600

    Переопределение id на Guid в IdentityRole

---
+++ OnlineCinema.Api/OnlineCinema.Data/Entities/RoleEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineCinema.Data.Entities
+{
+    /// <summary>
+    /// Представляет сущность роли в приложении.
+    /// Наследование от IdentityRole с переопределением ID на Guid.
+    /// </summary>
+    public class RoleEntity : IdentityRole<Guid>
+    {
+    }
+}
