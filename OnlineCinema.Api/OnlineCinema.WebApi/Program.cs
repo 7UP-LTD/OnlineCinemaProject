cdcf542770a8a4261ccb46c4ed5cb0c1509ecbe9
IliaKim91 <paterfamelias@gmail.com>
2023-05-12 17:52:56 +0600

    Пробую исправить комментарии

--- OnlineCinema.Api/OnlineCinema.WebApi/Program.cs
+++ OnlineCinema.Api/OnlineCinema.WebApi/Program.cs
@@ -29,15 +29,12 @@ namespace OnlineCinema.WebApi
                 c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
             });
 
-            // Add services to the container.
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
