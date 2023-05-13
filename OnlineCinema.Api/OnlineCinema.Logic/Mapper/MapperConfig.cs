5d9625ff8db32a53b9ac74900971444e5657098b
IliaKim91 <paterfamelias@gmail.com>
2023-05-13 19:39:34 +0600

    Конфигурация маппера

---
+++ OnlineCinema.Api/OnlineCinema.Logic/Mapper/MapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OnlineCinema.Data.Entities;
+using OnlineCinema.Logic.Dtos.AuthDtos;
+
+namespace OnlineCinema.Logic.Mapper
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<RegisterUserDto, UserEntity>()
+                .ForMember(src => src.NormalizedEmail, opt => opt.MapFrom(dest => dest.Email.ToUpper()))
+                .ForMember(src => src.NormalizedUserName, opt => opt.MapFrom(dest => dest.UserName.ToUpper()));
+        }
+    }
+}
