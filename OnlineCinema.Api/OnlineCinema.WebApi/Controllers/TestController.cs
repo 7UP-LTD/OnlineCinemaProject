5da66ab67c89f117bf86a2befcd48416d5bd3bb0
IliaKim91 <paterfamelias@gmail.com>
2023-05-13 19:41:42 +0600

    Тестовый котроллер

---
+++ OnlineCinema.Api/OnlineCinema.WebApi/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineCinema.Logic.Dtos;
+using System.Security.Claims;
+
+namespace OnlineCinema.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [Authorize]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var user = User.FindFirst(ClaimTypes.NameIdentifier);
+            var userId = user.Value;
+            return Ok(new List<TestUser>
+            {
+                new TestUser {FirstName = "John", LastName = "Smith"},
+                new TestUser {FirstName = "Amely", LastName = "Blant"}
+            });
+        }
+    }
+}
