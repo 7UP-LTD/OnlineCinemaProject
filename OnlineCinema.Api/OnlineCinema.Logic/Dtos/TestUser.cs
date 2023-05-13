c4e663539c851d1f95b623ffe317e3a3917198ee
IliaKim91 <paterfamelias@gmail.com>
2023-05-13 19:39:18 +0600

    Для теста

---
+++ OnlineCinema.Api/OnlineCinema.Logic/Dtos/TestUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCinema.Logic.Dtos
+{
+    public class TestUser
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+    }
+}
