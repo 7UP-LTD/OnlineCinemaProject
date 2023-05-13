478913c403df80584e9fa393089c8cfb4a3b0a53
IliaKim91 <paterfamelias@gmail.com>
2023-05-13 19:37:55 +0600

    Dto модель для входа

---
+++ OnlineCinema.Api/OnlineCinema.Logic/Dtos/AuthDtos/LoginUserDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineCinema.Logic.Dtos.AuthDtos
+{
+    /// <summary>
+    /// DTO модель для входа в приложение.
+    /// </summary>
+    public class LoginUserDto
+    {
+        /// <summary>
+        /// Имя пользователя. Обязательное поле
+        /// </summary>
+        [Required]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Длина пароля должна быть ")]
+        public string UserName { get; set; } = null!;
+
+        /// <summary>
+        /// Пароль обязательное поле. Минимальная длина пароля 6 символов.
+        /// </summary>
+        [Required]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Длина пароля должна быть не меньше 6 и не больше 50 символов.")]
+        public string Password { get; set; } = null!;
+    }
+}
