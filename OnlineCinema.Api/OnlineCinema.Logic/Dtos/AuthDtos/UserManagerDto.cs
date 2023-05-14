a416e8b1713144b6ddf74c5fe5905d72f645e4bb
IliaKim91 <paterfamelias@gmail.com>
2023-05-14 23:53:22 +0600

    Сменил имя

---
+++ OnlineCinema.Api/OnlineCinema.Logic/Dtos/AuthDtos/UserManagerDto.cs
@@ -0,0 +1,23 @@
+﻿namespace OnlineCinema.Logic.Dtos.AuthDtos
+{
+    /// <summary>
+    /// Ответ менеджера пользователей.
+    /// </summary>
+    public class UserManagerDto
+    {
+        /// <summary>
+        /// Сообщение об операции.
+        /// </summary>
+        public string? Message { get; set; }
+
+        /// <summary>
+        /// Успешность операции. По умолчанию false.
+        /// </summary>
+        public bool IsSuccess { get; set; } = false;
+
+        /// <summary>
+        /// Список ошибок.
+        /// </summary>
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
