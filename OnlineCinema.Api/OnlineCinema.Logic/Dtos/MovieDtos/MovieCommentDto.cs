3eefbc5ec0ac17f49d5a610ed6c96f836c5cdd4f
diniyuha <diniyuha@gmail.com>
2023-05-18 21:53:54 +0600

    исправление, для получения фильма со всеми данными (сезоны, эпизоды, комментарии, тэги, жанры)

--- OnlineCinema.Api/OnlineCinema.Logic/Dtos/MovieDtos/MovieCommentDto.cs
+++ OnlineCinema.Api/OnlineCinema.Logic/Dtos/MovieDtos/MovieCommentDto.cs
@@ -7,6 +7,6 @@ namespace OnlineCinema.Logic.Dtos.MovieDtos
         public string Text { get; set; }
         
         public Guid MovieId { get; set; }
-        public MovieDto Movie { get; set; }
+      
     }
 }
