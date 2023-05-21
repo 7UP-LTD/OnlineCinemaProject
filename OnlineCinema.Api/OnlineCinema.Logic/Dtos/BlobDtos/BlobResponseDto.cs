bf753923256862361c2cc0d1d4443e704b1693e9
IliaKim91 <paterfamelias@gmail.com>
2023-05-21 06:50:09 +0600

    Модель для ответа из сервиса Azure

---
+++ OnlineCinema.Api/OnlineCinema.Logic/Dtos/BlobDtos/BlobResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineCinema.Logic.Dtos.BlobDtos
+{
+    public class BlobResponseDto
+    {
+        public bool IsSuccess { get; set; }
+
+        public string? Url { get; set; }
+
+        public string? Errors { get; set; }
+    }
+}
