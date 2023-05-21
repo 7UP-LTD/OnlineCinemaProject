c6176b702cf9c81f4836557f22e3270f1bd0ab54
IliaKim91 <paterfamelias@gmail.com>
2023-05-21 06:49:43 +0600

    Внес изменения в базовый репозиторий

--- OnlineCinema.Api/OnlineCinema.Data/Repositories/BaseRepository.cs
+++ OnlineCinema.Api/OnlineCinema.Data/Repositories/BaseRepository.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using OnlineCinema.Data.Repositories.IRepositories;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Azure;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace OnlineCinema.Data.Repositories
 {
@@ -12,7 +15,7 @@ namespace OnlineCinema.Data.Repositories
     /// Реализация базового репозитория <see cref="IBaseRepository{T}"/> для работы с сущностями типа <typeparamref name="T"/>.
     /// </summary>
     /// <typeparam name="T">Тип сущности, с которой работает репозиторий.</typeparam>
-    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    public partial class BaseRepository<T> : IBaseRepository<T> where T : class
     {
         private readonly ApplicationDbContext _context;
         private readonly DbSet<T> dbSet;
@@ -20,7 +23,7 @@ namespace OnlineCinema.Data.Repositories
         /// <summary>
         /// Инициализирует новый экземпляр класса <see cref="BaseRepository{T}"/>.
         /// </summary>
-        /// <param name="db">Контекст базы данных <see cref="ApplicationDbContext"/>.</param>
+        /// <param name="context">Контекст базы данных <see cref="ApplicationDbContext"/>.</param>
         public BaseRepository(ApplicationDbContext context)
         {
             _context = context;
@@ -29,20 +32,46 @@ namespace OnlineCinema.Data.Repositories
 
         /// <inheritdoc/>
         public async Task<IEnumerable<T>> GetAllAsync(
-            Expression<Func<T, bool>>? filter = null, 
-            bool tracked = false, 
+            Expression<Func<T, bool>>? filter = null,
             string? includeProperty = null)
         {
-            IQueryable<T> query = dbSet;
-            if (!tracked)
-                query = query.AsNoTracking();
-
+            IQueryable<T> query = dbSet.AsNoTracking();
             if (includeProperty != null)
-                foreach (var includeProp in includeProperty.Split(',',
-                    StringSplitOptions.RemoveEmptyEntries))
+                foreach (var includeProp in includeProperty.Split(',', StringSplitOptions.RemoveEmptyEntries))
                     query = query.Include(includeProp);
-                
+
+            if (filter != null)
+                query = query.Where(filter);
+
             return await query.ToListAsync();
+        }
+
+        /// <inheritdoc/>
+        public async Task<PagingEntity<T>> GetPageEntitiesAsync(
+            Expression<Func<T, bool>>? filter = null,
+            string? includeProperty = null,
+            int currentPage = 1, 
+            int tEntityPerPage = 100)
+        {
+            IQueryable<T> query = dbSet.AsNoTracking();
+            if (includeProperty != null)
+                foreach (var includeProp in includeProperty.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                    query = query.Include(includeProp);
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            var totalEntity = query.Count();
+            if (tEntityPerPage > 0)
+            {
+                if (tEntityPerPage > 100)
+                    tEntityPerPage = 100;
+
+                query = query.Skip(tEntityPerPage * (currentPage - 1)).Take(tEntityPerPage);
+            }
+
+            var tEntityItems = await query.ToListAsync();
+            return new PagingEntity<T>(tEntityItems, totalEntity, tEntityPerPage);
         }
 
         /// <inheritdoc/>
@@ -55,13 +84,13 @@ namespace OnlineCinema.Data.Repositories
             if (!tracked)
                 query = query.AsNoTracking();
 
+            if (includeProperty != null)
+                foreach (var includeProp in includeProperty.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                    query = query.Include(includeProp);
+
             if (filter != null)
                 query = query.Where(filter);
 
-            if (includeProperty != null)
-                foreach (var includeProp in includeProperty.Split(',',
-                    StringSplitOptions.RemoveEmptyEntries))
-                    query = query.Include(includeProp);
             return await query.FirstOrDefaultAsync();
         }
 
